11a2637bf799966978a0b6d13b5a6fb1c6f42fa9
praneethmorla <praneethmorla@users.noreply.github.com>
2026-07-12 15:30:32 +0530

    Allow all CORS origins in Express app

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 export const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, limit: 300 }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
